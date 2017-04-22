70969209cf437cc4d4be5ce059ae20a708b0c8a6
Daniel Maldonado <daniel@danielmdesigns.com>
2017-04-22 17:37:54 -0500

    :zap: Gallery hover effects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
 	"use strict";
 
+//README
 $("#js-readme").on("click", function(e){
     e.preventDefault();
     $(".is-hidden").slideToggle(800);
     $(this).text($(this).text() === "Read Less" ? "Read More" : "Read Less");
 });
+	
+//GALLERY MOUSE HOVER
+$(".gallery-item > a > img").hover(function(){
+	$(this).stop().animate({"opacity": "1"}, "slow");
+},function(){
+	$(this).stop().animate({"opacity": "0.7"}, "slow");
+});
+
+//IF HANDHELD, ALL PHOTOS 100% OPACITY
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)){
+  $(".gallery-item > a > img").css({"opacity": "1"});
+}
 	
 });
